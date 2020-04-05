1fdc72573bb36953409d2beb1a51d9344c5d2e65
Martin Landart <martinlandart@gmail.com>
2020-04-05 13:32:10 -0300

    Added Client configuration for Client project

--- src/Marvin.IDP/Config.cs
+++ src/Marvin.IDP/Config.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
@@ -11,18 +12,38 @@ namespace Marvin.IDP
     {
         public static IEnumerable<IdentityResource> Ids =>
             new IdentityResource[]
-            { 
+            {
                 new IdentityResources.OpenId(),
                 new IdentityResources.Profile()
             };
 
         public static IEnumerable<ApiResource> Apis =>
-            new ApiResource[] 
+            new ApiResource[]
             { };
-        
+
         public static IEnumerable<Client> Clients =>
-            new Client[] 
-            { };
-        
+            new Client[]
+            {
+                new Client
+                {
+                    ClientName = "Image Gallery",
+                    ClientId = "imagegalleryclient",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = new List<string>()
+                    {
+                        "https://localhost:44389/signin-oidc"
+                    },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    },
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    }
+                }
+            };
+
     }
 }
