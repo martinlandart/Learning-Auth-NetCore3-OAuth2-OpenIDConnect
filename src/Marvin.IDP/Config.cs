13acc668e4799d86730c8caeeb645967e78be12f
Martin Landart <martinlandart@gmail.com>
2020-04-04 22:49:07 -0300

    First commit. Basic Identity Server running

---
+++ src/Marvin.IDP/Config.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace Marvin.IDP
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> Ids =>
+            new IdentityResource[]
+            { 
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+
+        public static IEnumerable<ApiResource> Apis =>
+            new ApiResource[] 
+            { };
+        
+        public static IEnumerable<Client> Clients =>
+            new Client[] 
+            { };
+        
+    }
+}
